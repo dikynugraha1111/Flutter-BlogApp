e9132e0e89ee47342fcdba23f6df06dbb3e050f2
nando <muhammad.rezki.ananda@gmail.com>
2021-10-14 09:51:01 +0700

    Add quarter circle widget

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog_app/shared/app_route.dart';
+import 'package:flutter_blog_app/shared/theme.dart';
+import 'package:flutter_blog_app/ui/widgets/main/quarter_circle_widget.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -6,7 +10,50 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 24),
+                  child: Image.asset(
+                    'assets/bitmap/logo.png',
+                    width: 72,
+                  ),
+                ),
+                InkWell(
+                  child: Stack(
+                    alignment: Alignment.centerRight,
+                    children: <Widget>[
+                      const SizedBox(
+                        width: 55,
+                        height: 55,
+                        child: QuarterCircle(
+                          circleAlignment: CircleAlignment.topRight,
+                          color: greyColor,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: SvgPicture.asset(
+                          'assets/vector/ic_switch_account.svg',
+                          width: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                  onTap: () {
+                    Navigator.pushReplacementNamed(
+                        context, AppRoute.loginRoute);
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
